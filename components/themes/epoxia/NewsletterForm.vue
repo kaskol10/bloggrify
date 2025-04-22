12986941d89d67a74cbb46d97c80e3f9832c68ca
kaskol10 <ramiroalvfer@gmail.com>
2025-04-22 12:00:04 +0200

    Adding all

--- components/themes/epoxia/NewsletterForm.vue
+++ components/themes/epoxia/NewsletterForm.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="newsletter-form">
-    <iframe
-      src="https://dashboard.mailerlite.com/forms/1455323/151766576435037399/share"
-      width="100%"
-      height="500"
-      style="border:0; width:100%;"
-      frameborder="0"
-    ></iframe>
+    <div class="ml-form-embed"
+      data-account="1455323"
+      data-form="151766576435037399">
+    </div>
   </div>
 </template>
 
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  // Cargar el script de MailerLite
+  const script = document.createElement('script')
+  script.src = "https://static.mailerlite.com/js/w/webforms.min.js"
+  script.type = "text/javascript"
+  document.body.appendChild(script)
+})
+</script>
+
 <style scoped>
 .newsletter-form {
-  @apply max-w-xl mx-auto my-8;
+  @apply max-w-xl mx-auto my-8 p-4;
 }
 </style> 
